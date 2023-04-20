820466799607a0c52382119bb872bea20339f23f
Ruslan Symonenko <symonenkors@gmail.com>
2023-04-20 17:37:27 +0300

    connected server to DB

--- server/index.js
+++ server/index.js
@@ -8,11 +8,21 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const DB_LOGIN = process.env.DB_LOGIN;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
 app.use(cors());
 app.use(express.json());
 
-function start () {
-  app.listen(PORT, () => {console.log(`Server is working on port: ${PORT}...`)});
+async function start () {
+  try {
+    await mongoose.connect(`mongodb+srv://${DB_LOGIN}:${DB_PASSWORD}@cluster0.oden63z.mongodb.net/test`)
+
+    app.listen(PORT, () => {console.log(`Server is working on port: ${PORT}...`)});
+
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 start();
